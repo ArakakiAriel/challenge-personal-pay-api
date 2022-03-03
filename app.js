f7fb65353de2f7721541d30725c1c0d2d46b08e2
ArakakiAriel <arakaki.arielkenji@gmail.com>
2022-03-03 10:40:28 -0300

    ?

---
+++ app.js
@@ -0,0 +1,62 @@
+const colors = require('colors');
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const config = require('./app/config/config'); //Archivo de configuraciones, de esta forma se levanta automáticamente y lo corre
+
+const app = express();
+const databaseRoute = require('./app/routes/database-route');
+const infoRoute = require('./app/routes/info-route');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false })); //Middleware cuando es app.use
+// parse application/json
+app.use(bodyParser.json());
+
+//Configuracion globar de rutas
+//app.use(require('./routes/index'));
+
+
+const mongoOptions = {
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology: true 
+}
+mongoose.connect(process.env.URLDB, mongoOptions);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+
+
+mongoose.connection.on('connected', () => {
+    console.log('MONGO CONNNECTED'.green);
+  });
+  
+mongoose.connection.on('disconnected', () => {
+    console.log('MONGO DISCONNECTED'.red);
+    if (mongoose.connection.readyState === 0) {
+      mongoose.connection.readyState = 2;
+      setTimeout(() => {
+        mongoose.connect(process.env.URLDB, mongoOptions);
+      }, config.mongo.reconnection_interval);
+    }
+});
+
+
+// covid routing
+//app.use(`/api/covid/database`, databaseRoute);
+app.use(`/api/covid/info`, infoRoute);
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Listening on port ", process.env.PORT);
+});
+
+
+(() => {
+    // if one of the dependencies does not response the ms can not start
+    require('./app/services/redis/init-redis-connection'); // eslint-disable-line
+  })();
+  
---
+++ app/config/config.js
@@ -0,0 +1,51 @@
+
+//===============
+//    Puerto
+//===============
+process.env.PORT = process.env.PORT || 3000;
+
+
+//===============
+//    Entorno
+//===============
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//=========================
+//  Google Client ID
+//========================
+process.env.GOOGLE_APPLICATION_CREDENTIALS = process.env.GOOGLE_APPLICATION_CREDENTIALS || './credentials/credentialAccess.json';
+
+//===============
+//   DataBase
+// Acá vamos a encontrar como configurar nuestra base de datos creada en MongoDB Atlas para poder almacenar allí la información
+//===============
+let urlDB;
+
+if( process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27018/someCollection'
+}else{
+    urlDB = 'yourMongoAtlasDBConnection'
+}
+
+process.env.URLDB = urlDB;
+
+module.exports = {
+    mongo: {
+      reconnection_interval: process.env.MONGO_RECONNECTION_INTERVAL || 3000
+    },
+    timeouts: {
+        timer_default: process.env.TIMEOUT_TIMER_DEFAULT || 3000,
+        timer_post_mongo: process.env.TIMER_POST_MONGO || 5000,
+
+    },
+    redisConfig: {
+        port: process.env.NODE_REDIS_PORT || 6379,
+        server: process.env.NODE_REDIS_SERVER || '127.0.0.1',
+        dbNumber: process.env.NODE_REDIS_DB_NUMBER || 15,
+        pass: process.env.NODE_REDIS_PASS || '',
+        ttl: process.env.NODE_REDIS_TTL || 86400,
+        tls: process.env.REDIS_USE_TLS
+    },
+};
+  
